358985b5e0e4fb63b67ca6368dff677dcb530975
Alex Junior Kuss <84691695+alexjuniorkuss@users.noreply.github.com>
2021-08-02 01:38:11 -0300

    paginas quase prontas

---
+++ 4_JS/main.js
@@ -0,0 +1,11 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    SlidesToShow: 1,
+    adaptativeHeight: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
